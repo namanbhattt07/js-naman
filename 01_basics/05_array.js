521cec7057bfa3928748dc2161340c0edd188f92
Naman Bhatt <namanbhatt277@gmail.com>
2026-01-11 07:09:23 +0000

    arrays

--- 01_basics/05_array.js
+++ 01_basics/05_array.js
@@ -1,12 +1,26 @@
 const myarr = [0,1,2,3,4,5]
 
-const ma1 = myarr.slice(1,3) // slice bas show karta hai 
-console.log(ma1);
-console.log("A",myarr);
 
-const ma2 = myarr.splice(1,3) // splice array se hi nikal deta hai vo values 
-console.log(ma2);
-console.log("B",myarr);
+//const ma1 = myarr.slice(1,3) // slice bas show karta hai 
+//console.log(ma1);
+//console.log("A",myarr);
 
+//const ma2 = myarr.splice(1,3) // splice array se hi nikal deta hai vo values 
+//console.log(ma2);
+//console.log("B",myarr);
 
+const myarr2 = [6,7,8,9]
+//myarr.push(myarr2) // list of list ban jati hai 
+//console.log(myarr);
+
+//const anontherarr = myarr.concat(myarr2)
+// console.log(anontherarr); // ye do array ko ache se join karta hai har ek ele
+
+//const spreadarr = [...myarr, ...myarr2]
+//console.log(spreadarr); // zaida preff hai kyuki ek se zaida array ko kar sakta hai spreaed
+
+/*const ekaurarr = [1,2,3,4,5,[3,45,5],5,[56,66,[43,3]],90] // ab ye array ke ander array ke ander array hai
+const flatarr = ekaurarr.flat(Infinity)
+console.log(flatarr);
+*/
 
