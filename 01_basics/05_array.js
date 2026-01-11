3f1473effa1da83f8fde807f415d7e37e23f81e4
Naman Bhatt <namanbhatt277@gmail.com>
2026-01-11 06:27:27 +0000

    array

---
+++ 01_basics/05_array.js
@@ -0,0 +1,12 @@
+const myarr = [0,1,2,3,4,5]
+
+const ma1 = myarr.slice(1,3) // slice bas show karta hai 
+console.log(ma1);
+console.log("A",myarr);
+
+const ma2 = myarr.splice(1,3) // splice array se hi nikal deta hai vo values 
+console.log(ma2);
+console.log("B",myarr);
+
+
+
